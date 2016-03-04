9d5dc009abdf9882255fb1c6257d943f8e821263
lexria <alexandriamlewis@me.com>
2016-03-04 14:42:20 -0500

    got the dashboard into alpha

---
+++ _templates.js
@@ -0,0 +1,28 @@
+var templates = {
+  recipeCardBig: [
+    '<article class="recipeCard">',
+      '<div class="title">',
+        '<h2>Rockin Chicken</h2>',
+      '</div>',
+      '<div class="description">',
+        '<h6> <%= description %> </h6>',
+      '</div>',
+      '<div class="cardLeft">',
+          '<h4>Ingredients: </h4>',
+          '<div class="ingredients">',
+            '<p> <%= ingredients %> </p>',
+          '</div>',
+          '<div class="times">',
+            '<h4>Prep Time: </h4><div class="prep"><p> <%= prepTime %> </p></div>',
+            '<h4>Cook Time: </h4><div class="cook"><p> <%= cookTime %> </p></div>',
+          '</div>',
+      '</div>',
+      '<div class="cardRight">',
+          '<h4>Directions: </h4>',
+          '<div class="directions">',
+            '<p> <%= directions %> </p>',
+          '</div>',
+      '</div>',
+    '</article>'
+  ].join('')
+}
