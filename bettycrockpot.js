997f8e577b72d8603dd990c247cad9061a4e16c9
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-03-04 10:22:49 -0500

    still playing with getting the api data

--- bettycrockpot.js
+++ bettycrockpot.js
@@ -3,23 +3,28 @@ $(document).ready(function(){
 })
 
 var page = {
-  url: 'http://api.bigoven.com/recipe/47725?api_key=',
-  apiKey: 'i4mh8L358389f4Tfsq82EWZUUlO9268k',
+  url: 'http://api.bigoven.com/recipes',
+  keyWord: '?title_kw=chicken',
+  // recipeId: '05734',
+  // apiKey: '?api_key=i4mh8L358389f4Tfsq82EWZUUlO9268k',
   init: function() {
     page.initStyling();
     page.initEvents();
   },
   initStyling: function(){
-
+    page.getRecipeData();
   },
   initEvents: function() {
 
   },
-  // BUILD URL
+
+  // BUILD URL //
   buildUrl: function() {
-    return page.url + page.apiKey;
+    return page.url + page.keyWord;
   },
-  getData: function() {
+
+  // GET DATA AJAX CALL //
+  getRecipeData: function() {
     $.ajax({
       method: 'GET',
       url: page.buildUrl(),
@@ -31,9 +36,16 @@ var page = {
       error: function(err) {
         alert('There was an error');
       }
-
     })
   },
+
+  // ADD DATA TO PAGE //
+  // addDataToPage: function() {
+  //
+  // },
+
+
+
 
 
 
