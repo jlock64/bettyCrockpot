885f4819bfe90d8c73183eb4856a1f632e9da486
drtonton <tkfoster26@gmail.com>
2016-03-04 12:52:31 -0500

    added get/setters and constructor to user class

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,8 @@ public class Main {
         stmt.execute("CREATE TABLE IF NOT EXISTS user (user_Id INT, user_name VARCHAR, password VARCHAR)");
         stmt.execute("CREATE TABLE IF NOT EXISTS recipe (recipe_id INT, recipe_join-id INT, recipe_name");
     }
+
+    public static void main(String[] args) {
+
+    }
 }
